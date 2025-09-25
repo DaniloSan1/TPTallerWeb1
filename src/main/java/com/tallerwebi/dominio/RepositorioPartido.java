3e236eebd161279cbeb5a87c73deb92d67765053
moracicero <135073675+moracicero@users.noreply.github.com>
2025-09-25 18:16:58 -0300

    Create RepositorioPartido.java

---
+++ src/main/java/com/tallerwebi/dominio/RepositorioPartido.java
@@ -0,0 +1,8 @@
+package com.tallerwebi.dominio;
+import java.util.List;
+
+public interface RepositorioPartido {
+    List<Partido> todos();
+    Partido porId(Long id);
+    void guardar(Partido partido);
+}
