40f88ef7eebb3474f33030eaab0f8661290e4bb3
FelipeSguerzo <fsguerzo231@alumno.unlam.edu.ar>
2025-10-30 17:37:21 -0300

    Agregue funcionalidad de editarPerfil

--- src/main/java/com/tallerwebi/presentacion/ControladorUsuario.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorUsuario.java
@@ -1,23 +1,27 @@
 package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.ServicioLogin;
+import com.tallerwebi.dominio.ServicioUsuario;
 import com.tallerwebi.dominio.Usuario;
+import com.tallerwebi.dominio.excepcion.UsernameExistenteException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/perfil")
 public class ControladorUsuario {
+    private final ServicioUsuario servicioUsuario;
     private ServicioLogin servicioLogin;
 
-    public ControladorUsuario(ServicioLogin servicioLogin) {
+    public ControladorUsuario(ServicioLogin servicioLogin, ServicioUsuario servicioUsuario) {
         this.servicioLogin = servicioLogin;
+        this.servicioUsuario = servicioUsuario;
     }
 
     @GetMapping("/ver/id/{id}")
@@ -67,4 +71,46 @@ public class ControladorUsuario {
         request.getSession().invalidate();
         return new ModelAndView("redirect:/login");
     }
+
+    @GetMapping("/editar")
+    public String editar(ModelMap modelo, HttpServletRequest request) {
+        String email = (String) request.getSession().getAttribute("EMAIL");
+        if (email != null) {
+            Usuario usuario = servicioLogin.buscarPorEmail(email);
+            modelo.addAttribute("usuario", usuario);
+            return "editarPerfil";
+        }
+
+        return "redirect:/login";
+    }
+
+    @PostMapping("/editar")
+    public String guardarCambios(@ModelAttribute("usuario") Usuario usuarioEditado, HttpServletRequest request, ModelMap modelo) {
+        String email = (String) request.getSession().getAttribute("EMAIL");
+        if (email != null) {
+            try {
+                Usuario usuario = servicioLogin.buscarPorEmail(email);
+                Usuario usuarioExistente = servicioUsuario.buscarPorUsername(usuarioEditado.getUsername());
+                if (usuarioExistente != null && !usuarioExistente.getId().equals(usuarioEditado.getId())) {
+                    throw new UsernameExistenteException();
+                }
+
+                usuario.setUsername(usuarioEditado.getUsername());
+                usuario.setNombre(usuarioEditado.getNombre());
+                usuario.setApellido(usuarioEditado.getApellido());
+                usuario.setPosicionFavorita(usuarioEditado.getPosicionFavorita());
+
+                servicioUsuario.modificarUsuario(usuario);
+
+                return "redirect:/perfil";
+
+            } catch (UsernameExistenteException e) {
+                modelo.addAttribute("usuario", usuarioEditado);
+                modelo.addAttribute("error", e.getMessage());
+                return "editarPerfil";
+            }
+
+        }
+            return "redirect:/login";
+        }
 }
--- src/test/java/com/tallerwebi/dominio/ServicioUsuarioImplTest.java
+++ src/test/java/com/tallerwebi/dominio/ServicioUsuarioImplTest.java
@@ -9,8 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ServicioUsuarioImplTest {
     private ServicioUsuario servicioUsuarioImpl;
@@ -77,5 +76,57 @@ public class ServicioUsuarioImplTest {
 
 
     //Test de modificar usuario pendiente hasta que implemente la parte de editar perfil
+    @Test
+    public void queSePuedaModificarUnUsuarioCorrectamente() {
+        // Dado
+        Usuario usuarioOriginal = new Usuario();
+        usuarioOriginal.setId(1L);
+        usuarioOriginal.setEmail("email@email.com");
+        usuarioOriginal.setUsername("usuarioViejo");
+        usuarioOriginal.setNombre("NombreViejo");
+        usuarioOriginal.setApellido("ApellidoViejo");
+        usuarioOriginal.setPosicionFavorita("Defensa");
 
+        Usuario usuarioModificado = new Usuario();
+        usuarioModificado.setId(1L);
+        usuarioModificado.setEmail("email@email.com"); // El email no cambia
+        usuarioModificado.setUsername("usuarioNuevo");
+        usuarioModificado.setNombre("NombreNuevo");
+        usuarioModificado.setApellido("ApellidoNuevo");
+        usuarioModificado.setPosicionFavorita("Delantero");
+
+
+        when(repositorioUsuario.buscar("email@email.com")).thenReturn(usuarioModificado);
+
+
+        servicioUsuarioImpl.modificarUsuario(usuarioModificado);
+
+
+        verify(repositorioUsuario).modificar(usuarioModificado);
+
+
+        Usuario usuarioActualizado = repositorioUsuario.buscar("email@email.com");
+        assertThat(usuarioActualizado.getUsername(), equalTo("usuarioNuevo"));
+        assertThat(usuarioActualizado.getNombre(), equalTo("NombreNuevo"));
+        assertThat(usuarioActualizado.getApellido(), equalTo("ApellidoNuevo"));
+        assertThat(usuarioActualizado.getPosicionFavorita(), equalTo("Delantero"));
+    }
+
+    @Test
+    public void queAlBuscarPorUsernameDevuelveUnUsuarioCorrectamente() {
+        Usuario usuarioOriginal = new Usuario("nombre","contraseña","email","username");
+        usuarioOriginal.setId(1L);
+        when(repositorioUsuario.buscar("username")).thenReturn(usuarioOriginal);
+        Usuario usuarioBuscado =  repositorioUsuario.buscar("username");
+        assertThat(usuarioOriginal, equalTo(usuarioBuscado));
+    }
+
+    @Test
+    public void queAlBuscarPorIdDevuelvaUnUsuarioCorrectamente(){
+        Usuario usuarioOriginal = new Usuario("nombre","contraseña","email","username");
+        usuarioOriginal.setId(1L);
+        when(repositorioUsuario.buscarPorId(1L)).thenReturn(usuarioOriginal);
+        Usuario usuarioBuscado =  repositorioUsuario.buscarPorId(1L);
+        assertThat(usuarioOriginal, equalTo(usuarioBuscado));
+    }
 }
