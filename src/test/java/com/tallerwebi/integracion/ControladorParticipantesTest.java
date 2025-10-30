1cddbaf8fc1c92b86c75c55acb4b1243456ffe23
Edgar Elián Rodriguez <erodriguez@alumno.unlam.edu.ar>
2025-10-30 17:15:48 -0300

    Arreglar deberiaActualizarEquipoSiExisteElParticipante test

--- src/test/java/com/tallerwebi/integracion/ControladorParticipantesTest.java
+++ src/test/java/com/tallerwebi/integracion/ControladorParticipantesTest.java
@@ -45,95 +45,95 @@ import com.tallerwebi.integracion.config.SpringWebTestConfig;
 @ContextConfiguration(classes = { SpringWebTestConfig.class, HibernateTestConfig.class })
 public class ControladorParticipantesTest {
 
-    @Autowired
-    private WebApplicationContext wac;
-    private MockMvc mockMvc;
+        @Autowired
+        private WebApplicationContext wac;
+        private MockMvc mockMvc;
 
-    @Autowired
-    private SessionFactory sessionFactory;
+        @Autowired
+        private SessionFactory sessionFactory;
 
-    @BeforeEach
-    public void init() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
+        @BeforeEach
+        public void init() {
+                this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+        }
 
-    @Test
-    @Transactional
-    @Rollback
-    public void deberiaActualizarEquipoSiExisteElParticipante() throws Exception {
-        Cancha cancha = new Cancha("Cancha 1", "Direccion 1", 20, "CESPED", Zona.NORTE);
-        this.sessionFactory.getCurrentSession().save(cancha);
+        @Test
+        @Transactional
+        @Rollback
+        public void deberiaActualizarEquipoSiExisteElParticipante() throws Exception {
+                Cancha cancha = new Cancha("Cancha 1", "Direccion 1", 20, "CESPED", Zona.NORTE);
+                this.sessionFactory.getCurrentSession().save(cancha);
 
-        Horario horario = new Horario(cancha, DayOfWeek.MONDAY, LocalTime.now(), LocalTime.now().plusHours(1));
-        this.sessionFactory.getCurrentSession().save(horario);
+                Horario horario = new Horario(cancha, DayOfWeek.MONDAY, LocalTime.now(), LocalTime.now().plusHours(1));
+                this.sessionFactory.getCurrentSession().save(horario);
 
-        Usuario creador = new Usuario("usuario1", "password", "email@example.com","usernameCreador");
-        creador.setApellido("apellido");
-        this.sessionFactory.getCurrentSession().save(creador);
+                Usuario creador = new Usuario("usuario1", "password", "email@example.com", "usernameCreador");
+                creador.setApellido("apellido");
+                this.sessionFactory.getCurrentSession().save(creador);
 
-        Reserva reserva = new Reserva(horario, creador, LocalDateTime.now().plusDays(1));
-        this.sessionFactory.getCurrentSession().save(reserva);
+                Reserva reserva = new Reserva(horario, creador, LocalDateTime.now().plusDays(1));
+                this.sessionFactory.getCurrentSession().save(reserva);
 
-        Partido nuevoPartido = new Partido(null, "Partido de prueba", "Descripción del partido",
-                Nivel.INTERMEDIO,
-                10,
-                reserva,
-                creador);
+                Partido nuevoPartido = new Partido(null, "Partido de prueba", "Descripción del partido",
+                                Nivel.INTERMEDIO,
+                                10,
+                                reserva,
+                                creador);
 
-        this.sessionFactory.getCurrentSession().save(nuevoPartido);
+                this.sessionFactory.getCurrentSession().save(nuevoPartido);
 
-        Usuario participante = new Usuario("usuario2", "password2", "email2@example.com","username2");
-        participante.setNombre("participante1");
-        participante.setApellido("apellido1");
-        this.sessionFactory.getCurrentSession().save(participante);
+                Usuario participante = new Usuario("usuario2", "password2", "email2@example.com", "username2");
+                participante.setNombre("participante1");
+                participante.setApellido("apellido1");
+                this.sessionFactory.getCurrentSession().save(participante);
 
-        PartidoParticipante partidoParticipante = new PartidoParticipante(nuevoPartido, participante,
-                Equipo.SIN_EQUIPO);
-        partidoParticipante.setId(1L);
-        this.sessionFactory.getCurrentSession().save(partidoParticipante);
-        this.sessionFactory.getCurrentSession().flush();
+                PartidoParticipante partidoParticipante = new PartidoParticipante(nuevoPartido, participante,
+                                Equipo.SIN_EQUIPO);
+                this.sessionFactory.getCurrentSession().save(partidoParticipante);
+                this.sessionFactory.getCurrentSession().flush();
 
-        ResultActions result = mockMvc.perform(post("/participantes/1/asignacion-equipo")
-                .param("equipo", "EQUIPO_1")
-                .sessionAttr("EMAIL", "test@example.com")
-                .header("referer", "/partido/1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/partido/1"));
+                ResultActions result = mockMvc
+                                .perform(post("/participantes/" + partidoParticipante.getId() + "/asignacion-equipo")
+                                                .param("equipo", "EQUIPO_1")
+                                                .sessionAttr("EMAIL", "test@example.com")
+                                                .header("referer", "/partido/1"))
+                                .andExpect(status().is3xxRedirection())
+                                .andExpect(redirectedUrl("/partido/1"));
 
-        String FlashErrorMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesError");
-        assertNull(FlashErrorMessage);
+                String FlashErrorMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesError");
+                assertNull(FlashErrorMessage);
 
-        String flashSuccessMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesSuccess");
-        assertThat(flashSuccessMessage, is("Equipo asignado correctamente"));
+                String flashSuccessMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesSuccess");
+                assertThat(flashSuccessMessage, is("Equipo asignado correctamente"));
 
-        PartidoParticipante partidoParticipanteActualizado = sessionFactory.getCurrentSession()
-                .get(PartidoParticipante.class, 1L);
-        assertNotNull(partidoParticipanteActualizado);
-        assertThat(partidoParticipanteActualizado.getEquipo(), is(Equipo.EQUIPO_1));
-    }
+                PartidoParticipante partidoParticipanteActualizado = sessionFactory.getCurrentSession()
+                                .get(PartidoParticipante.class, partidoParticipante.getId());
+                assertNotNull(partidoParticipanteActualizado);
+                assertThat(partidoParticipanteActualizado.getEquipo(), is(Equipo.EQUIPO_1));
+        }
 
-    @Test
-    public void debeRetornarMensajeDeErrorCuandoNoExisteElParticipante() throws Exception {
-        ResultActions result = mockMvc.perform(post("/participantes/1/asignacion-equipo")
-                .param("equipo", "EQUIPO_1")
-                .sessionAttr("EMAIL", "test@example.com")
-                .header("referer", "/partido/1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/partido/1"));
+        @Test
+        public void debeRetornarMensajeDeErrorCuandoNoExisteElParticipante() throws Exception {
+                ResultActions result = mockMvc.perform(post("/participantes/1/asignacion-equipo")
+                                .param("equipo", "EQUIPO_1")
+                                .sessionAttr("EMAIL", "test@example.com")
+                                .header("referer", "/partido/1"))
+                                .andExpect(status().is3xxRedirection())
+                                .andExpect(redirectedUrl("/partido/1"));
 
-        String flashErrorMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesError");
-        assertThat(flashErrorMessage, is("No se encontró el participante"));
+                String flashErrorMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesError");
+                assertThat(flashErrorMessage, is("No se encontró el participante"));
 
-        String flashSuccessMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesSuccess");
-        assertNull(flashSuccessMessage);
-    }
+                String flashSuccessMessage = (String) result.andReturn().getFlashMap().get("listaParticipantesSuccess");
+                assertNull(flashSuccessMessage);
+        }
 
-    @Test
-    public void deberiaRedirigirAlLoginSiNoHaySesion() throws Exception {
-        mockMvc.perform(post("/participantes/1/asignacion-equipo")
-                .param("equipo", "A")
-                .header("referer", "/partido/1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/login"));
-    }
+        @Test
+        public void deberiaRedirigirAlLoginSiNoHaySesion() throws Exception {
+                mockMvc.perform(post("/participantes/1/asignacion-equipo")
+                                .param("equipo", "A")
+                                .header("referer", "/partido/1"))
+                                .andExpect(status().is3xxRedirection())
+                                .andExpect(redirectedUrl("/login"));
+        }
 }
