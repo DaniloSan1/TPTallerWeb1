8da21459d3774180836ce971d53819d8bfe17fe1
Elián Rodríguez <38392719+rodriguezEdgar@users.noreply.github.com>
2025-11-05 16:03:32 -0300

    Agregar la vista de "Mis Equipos" (#17)
    Co-authored-by: Edgar Elián Rodriguez <erodriguez@alumno.unlam.edu.ar>

--- src/main/java/com/tallerwebi/dominio/ServicioEquipo.java
+++ src/main/java/com/tallerwebi/dominio/ServicioEquipo.java
@@ -3,6 +3,8 @@ package com.tallerwebi.dominio;
 import com.tallerwebi.dominio.excepcion.EquipoNoEncontrado;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface ServicioEquipo {
     Equipo crearEquipo(String nombre, Usuario creador);
@@ -10,4 +12,8 @@ public interface ServicioEquipo {
     Equipo buscarPorId(Long id) throws EquipoNoEncontrado;
 
     void actualizarNombre(Equipo equipo, String nuevoNombre);
+
+    List<Equipo> obtenerEquiposDelUsuario(Usuario usuario);
+
+    List<Equipo> obtenerEquiposDelUsuarioConFiltro(Usuario usuario, String nombre);
 }
