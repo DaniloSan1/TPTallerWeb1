10e0dd16e03071b25c7dfcc33cadcd7b7ce92349
Edgar Elián Rodriguez <erodriguez@alumno.unlam.edu.ar>
2025-11-06 17:10:46 -0300

    Configurar utf-8 en Thymeleaf

--- src/main/java/com/tallerwebi/config/SpringWebConfig.java
+++ src/main/java/com/tallerwebi/config/SpringWebConfig.java
@@ -46,6 +46,7 @@ public class SpringWebConfig implements WebMvcConfigurer {
         // Template cache is true by default. Set to false if you want
         // templates to be automatically updated when modified.
         templateResolver.setCacheable(true);
+        templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
 
@@ -71,6 +72,7 @@ public class SpringWebConfig implements WebMvcConfigurer {
     public ThymeleafViewResolver viewResolver() {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
         return viewResolver;
     }
 
